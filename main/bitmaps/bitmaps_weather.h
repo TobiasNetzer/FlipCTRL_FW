e4379ef2bd8837161a0b41da9da0484fef0af5dd
Tobias Netzer <Tobias.Netzer@web.de>
2025-07-20 17:25:06 +0200

    add weather bitmaps

---
+++ main/bitmaps/bitmaps_weather.h
@@ -0,0 +1,33 @@
+#ifndef __BITMAPS_WEATHER_H__
+#define __BITMAPS_WEATHER_H__
+#ifdef __cplusplus
+extern "C" {
+#endif /*__cplusplus */
+
+#include <stdint.h>
+
+extern const uint16_t weather_clear_day[];
+
+extern const uint16_t weather_clear_night[];
+
+extern const uint16_t weather_few_clouds_day[];
+
+extern const uint16_t weather_few_clouds_night[];
+
+extern const uint16_t weather_scattered_clouds[];
+
+extern const uint16_t weather_broken_clouds[];
+
+extern const uint16_t weather_rain[];
+
+extern const uint16_t weather_thunderstorm[];
+
+extern const uint16_t weather_snow[];
+
+extern const uint16_t weather_mist[];
+
+#ifdef __cplusplus
+}
+#endif /*__cplusplus */
+
+#endif /* __BITMAPS_WEATHER_H__ */
--- main/main.c
+++ main/main.c
@@ -5,29 +5,25 @@
 #include "flipdot_driver.h"
 #include "flipdot_gfx.h"
 
-#include "bitmaps.h"
+#include "bitmaps/bitmaps_weather.h"
 
 void app_main(void) {
     flipdot_init();
+
+    const uint16_t* test_bitmaps[] = {weather_clear_day, weather_clear_night, weather_few_clouds_day, weather_few_clouds_night, weather_scattered_clouds, weather_broken_clouds, weather_rain, weather_thunderstorm, weather_snow, weather_mist};
     while(1) {
-        flipdot_clear();
-        flipdot_draw_bitmap(0,2,weather_rain,true);
-        flipdot_draw_text(12,4,"23");
-        flipdot_set_pixel(25,5,true);
-        flipdot_set_pixel(24,4,true);
-        flipdot_set_pixel(26,4,true);
-        flipdot_set_pixel(25,3,true);
-        flipdot_display();
-        vTaskDelay(pdMS_TO_TICKS(3000));
-        flipdot_clear();
-        flipdot_draw_bitmap(0,2,weather_cloudy,true);
-        flipdot_draw_text(12,4,"23");
-        flipdot_set_pixel(25,5,true);
-        flipdot_set_pixel(24,4,true);
-        flipdot_set_pixel(26,4,true);
-        flipdot_set_pixel(25,3,true);
-        flipdot_display();
-        vTaskDelay(pdMS_TO_TICKS(3000));
+
+        for(uint8_t i = 0; i<10; i++) {
+            flipdot_clear();
+            flipdot_draw_bitmap(0,2,test_bitmaps[i],true);
+            flipdot_draw_text(12,4,"23");
+            flipdot_set_pixel(25,5,true);
+            flipdot_set_pixel(24,4,true);
+            flipdot_set_pixel(26,4,true);
+            flipdot_set_pixel(25,3,true);
+            flipdot_display();
+            vTaskDelay(pdMS_TO_TICKS(3000));
+        }
     }
     
 }
